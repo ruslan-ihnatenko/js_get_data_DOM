f4f89cb0eadd69abbad1efa0a040707a139ab194
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2025-01-06 18:06:21 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+'use strict';
+const totalPopulationElement = document.getElementsByClassName('total-population')[0];
+const averagePopulationElement = document.getElementsByClassName('average-population')[0];
+const populations = [
+    ...document.getElementsByClassName('population')
+];
+const normalizedNumbers = populations.map((el)=>+el.innerHTML.split(',').join(''));
+const nfObject = new Intl.NumberFormat('en-US');
+const totalPopulation = normalizedNumbers.reduce((prev, curr)=>prev + curr, 0);
+const averagePopulation = +(totalPopulation / normalizedNumbers.length).toFixed(0);
+totalPopulationElement.innerHTML = `${nfObject.format(totalPopulation)}`;
+averagePopulationElement.innerHTML = `${nfObject.format(averagePopulation)}`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
